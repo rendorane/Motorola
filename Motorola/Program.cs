088655fdca97b43d597ad8b1df71939a72edd08a
Mariusz Dudek <mariuszd@agh.edu.pl>
2020-11-26 02:23:47 +0100

    Read .txt to list Randomly pick one line

--- Motorola/Program.cs
+++ Motorola/Program.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 
 namespace Motorola
 {
@@ -10,7 +11,16 @@ namespace Motorola
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            List<string> countriesAndCapitals = File.ReadLines(@"C:\\Users\\Marian\\source\repos\\Motorola\\Motorola\\countries_and_capitals.txt").ToList();
+            List<string> countries = new List<string>();
+            List<string> capitals = new List<string>();
 
-        }
+            Console.WriteLine(countriesAndCapitals[rnd.Next(0, countriesAndCapitals.Count)]);
+
+            Console.ReadLine();
+
+            
+    }
     }
 }
