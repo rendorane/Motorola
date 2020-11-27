a48f28a03b82eb9f4bf0b8205ceff0e31977e6ed
Mariusz Dudek <mariuszd@agh.edu.pl>
2020-11-27 01:05:06 +0100

    Add win and play again mechanism.

--- Motorola/Program.cs
+++ Motorola/Program.cs
@@ -11,13 +11,16 @@ namespace Motorola
     {
         static void Main()
         {
-            try
+            bool gameOn = true;
+
+            while (gameOn)
             {
                 List<string> countriesAndCapitals = File.ReadLines(@"C:\\Users\\Marian\\source\repos\\Motorola\\Motorola\\countries_and_capitals.txt").ToList();
                 Random rnd = new Random();
                 string pickedRecord = (countriesAndCapitals[rnd.Next(0, countriesAndCapitals.Count)]);
                 string[] splittedRecord = pickedRecord.Split('|');
-                string country = splittedRecord[0].Trim().ToLower();
+                string country = splittedRecord[0].Trim();
+                string capitalUpperCase = splittedRecord[1].Trim();
                 string capital = splittedRecord[1].Trim().ToLower();
                 int lifes = 5;
 
@@ -26,18 +29,16 @@ namespace Motorola
                 List<char> capitalLetters = new List<char>();
                 capitalLetters.AddRange(capital);
 
+
+
                 Console.WriteLine(capital);
-
-                bool gameOn = true;
-
+                Console.WriteLine("You have " + lifes + " lifes left");
                 Console.WriteLine("Name of the capital to guess:");
 
                 ShowCapitalName(capital, rightLetters);
 
-                while (gameOn)
+                while (true)
                 {
-                   
-
                     if (lifes <= 0)
                     {
                         Console.WriteLine("You lost");
@@ -48,9 +49,9 @@ namespace Motorola
                     {
                         Console.WriteLine("Hint: The capital of " + country);
                     }
-
                     Console.WriteLine();
-
+                    Console.WriteLine(rightLetters.Count);
+                    Console.WriteLine(capitalLetters.Count);
                     Console.Write("Would you like to guess a single letter (l), or give it a shot and try with entire word (w): ");
                     char choice = GetCharFromUser();
 
@@ -66,10 +67,24 @@ namespace Motorola
 
                             ShowCapitalName(capital, rightLetters);
 
+                            for (int i = 0; i < capitalLetters.Count; i++)
+                            {
+                                if (capitalLetters[i] == letter)
+                                {
+                                    capitalLetters.Remove(letter);
+                                }
+                            }
+
+                            if (capitalLetters.Count == 0) 
+                            {
+                                Console.WriteLine("You win!");
+                                break;
+                            }
                         }
                         else
                         {
                             Console.WriteLine("Oops! Try again!");
+
                             if (!wrongLetters.Contains(letter))
                             {
                                 wrongLetters.Add(letter);
@@ -80,46 +95,51 @@ namespace Motorola
                                 {
                                     Console.Write(wrongLetters[i] + ", ");
                                 }
+                                Console.WriteLine();
+                                Console.WriteLine("You have " + lifes + " lifes left");
                             }
 
                         }
-
                     }
                     else if (choice == 'w')
                     {
                         Console.Write("Enter correct answer: ");
-                        string word = (Console.ReadLine());
+                        string word = Console.ReadLine();
 
                         if (word == capital)
                         {
                             Console.WriteLine("Wow! That is correct! You win!");
-                            Console.WriteLine(capital + " is a capital of " + country);
+                            Console.WriteLine(capitalUpperCase + " is a capital of " + country);
+                            break;
                         }
                         else
                         {
                             Console.WriteLine("Oops! Try again!");
                             lifes -= 2;
+                            Console.WriteLine("You have " + lifes + " lifes left");
                         }
                     }
                     else
                     {
                         Console.WriteLine("Type 'l' or 'w'");
                     }
-
-               
-
                 }
+                Console.Write("Would you like to start again? (y/n)");
+                char end = GetCharFromUser();
 
-                Console.ReadLine();
-
-               }
-
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception {0}", e);
-                Console.ReadLine();
+                if (end == 'y')
+                {
+                    gameOn = true;
+                }
+                else if (end == 'n')
+                {
+                    gameOn = false;
+                }
+                else 
+                {
+                    Console.Write("y/n?");
+                }
             }
-
 
         }
 
@@ -138,18 +158,14 @@ namespace Motorola
                     else 
                     {
                         Console.WriteLine("Enter a single char");
-                    }
-                    
+                    }                  
                 }
                 else
                 {
                     Console.WriteLine("Enter a single char");
                 }
             }
-
-     
         }
-
         static void ShowCapitalName(string capital, List<char> rightLetters)
         {
             for (int i = 0; i < capital.Length; i++)
@@ -167,9 +183,9 @@ namespace Motorola
                     Console.Write(" _");
                 }
             }
-
             Console.WriteLine();
-        }
 
+        
+        }
     }
 }
